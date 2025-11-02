1d45b58986b15e984ffecf1459deb748db875cc1
J-hta-n <e0969121@u.nus.edu>
2025-11-02 15:18:28 +0800

    refactor: abstract gameNetAPI into sender and receiver

--- receiver.py
+++ receiver.py
@@ -1,17 +1,109 @@
-from gameNetAPI import UNRELIABLE_CHANNEL, GameNetAPI
+import socket
+import threading
 import time
+from collections import deque
+from utils import pack_packet, unpack_packet, now_ms
 
-api = GameNetAPI(('127.0.0.1', 12001), ('127.0.0.1', 11000), False)
+RETRANSMIT_TIMEOUT_MS = 200
+RELIABLE_CHANNEL = 0
+UNRELIABLE_CHANNEL = 1
+
+class Receiver:
+    def __init__(self, src_socket_addr, dest_socket_addr):
+        self.src_socket_addr = src_socket_addr
+        self.dest_socket_addr = dest_socket_addr
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(src_socket_addr)
+        self.sock.setblocking(False)
+
+        self.seq_to_recv = 0
+        self.unreliable_buffer = deque()  # Unreliable packets buffer: FIFO queue of (recv_seq, payload)
+        self.unreliable_seqs = set()  # To increment self.seq_to_recv if alr recv seq num
+        self.reliable_buffer = {}  # Reliable packets buffer: {recv_seq -> (payload, timestamp)}
+        self.last_missing_time = None
+
+        self.unreliable_data_lock = threading.Lock()
+        self.reliable_data_lock = threading.Lock()
+
+        self.running_threads = True
+        self.recv_thread = threading.Thread(target=self._recv_and_ack, daemon=True)
+        self.recv_thread.start()
+
+    def recv(self, block=False, timeout=None):
+        start = time.time()
+        while True:
+            with self.reliable_data_lock:
+                if self.seq_to_recv in self.reliable_buffer:
+                    payload, ts = self.reliable_buffer.pop(self.seq_to_recv)
+                    seq = self.seq_to_recv
+                    self.seq_to_recv = self._increment_seq(seq)
+                    self.last_missing_time = None
+                    return seq, RELIABLE_CHANNEL, payload
+
+                if (self.last_missing_time and 
+                    (now_ms() - self.last_missing_time) > RETRANSMIT_TIMEOUT_MS):
+                    print(f"[RECEIVER] skipping missing seq={self.seq_to_recv}")
+                    self.seq_to_recv = self._increment_seq(self.seq_to_recv)
+                    self.last_missing_time = now_ms()
+
+            with self.unreliable_data_lock:
+                if self.unreliable_buffer:
+                    if self.seq_to_recv in self.unreliable_seqs:
+                        self.unreliable_seqs.remove(self.seq_to_recv)
+                        self.seq_to_recv = self._increment_seq(self.seq_to_recv)
+                    seq, payload = self.unreliable_buffer.popleft()
+                    return seq, UNRELIABLE_CHANNEL, payload
+
+            if not block:
+                return None
+            if timeout and time.time() - start > timeout:
+                return None
+            time.sleep(0.01)
+
+    def close(self):
+        self.running_threads = False
+        self.sock.close()
+    
+    def _increment_seq(self, seq):
+        return (seq + 1) & 0xFFFF
+
+    def _recv_and_ack(self):
+        while self.running_threads:
+            try:
+                data, _ = self.sock.recvfrom(65536)
+            except BlockingIOError:
+                time.sleep(0.001)
+                continue
+            try:
+                ch, seq, ts, payload = unpack_packet(data)
+            except Exception:
+                continue
+            
+            if ch == RELIABLE_CHANNEL:
+                with self.reliable_data_lock:
+                    self.reliable_buffer[seq] = (payload, now_ms())
+                    if seq > self.seq_to_recv and self.last_missing_time is None:
+                        self.last_missing_time = now_ms()
+                
+                ack = pack_packet(RELIABLE_CHANNEL, seq, now_ms(), b"ACK")
+                self.sock.sendto(ack, self.dest_socket_addr)
+            else:
+                with self.unreliable_data_lock:
+                    self.unreliable_buffer.append((seq, payload))
+                    self.unreliable_seqs.add(seq)
+
+# Main receiver logic
 print("[RECEIVER] listening...")
+receiver = Receiver(('127.0.0.1', 12001), ('127.0.0.1', 11000))
 recv = []
 start_time = time.time()
 
 try:
     while time.time() - start_time < 15:
-        msg = api.recv(block=True, timeout=1)
+        msg = receiver.recv(block=True, timeout=1)
         if msg:
             seq, ch, data = msg
-            print(f"[RECEIVER] got seq={seq} ch={'REL' if ch==UNRELIABLE_CHANNEL else 'UNREL'} data={data}")
+            print(f"[RECEIVER] got seq={seq} ch={'REL' if ch==RELIABLE_CHANNEL else 'UNREL'} data={data}")
             recv.append((seq, ch))
 except KeyboardInterrupt:
     pass
@@ -22,7 +114,7 @@ finally:
     unrel_buffer = []
 
     for seq, ch in recv:
-        if ch == 1:
+        if ch == UNRELIABLE_CHANNEL:
             unrel_buffer.append(str(seq))
         else:
             if unrel_buffer:
@@ -35,4 +127,4 @@ finally:
         result.append(",".join(unrel_buffer))
 
     print(f"[RECEIVER] RESULT: {result}")
-    api.close()
+    receiver.close()
--- sender.py
+++ sender.py
@@ -1,18 +1,107 @@
-from gameNetAPI import GameNetAPI
-import time, random
+import socket
+import threading
+import time
+import random
+from utils import pack_packet, unpack_packet, now_ms
 
-api = GameNetAPI(('127.0.0.1', 12000), ('127.0.0.1', 11000), True)
+RETRANSMIT_TIMEOUT_MS = 200  # Timeout t beyond which reliable packet is dropped = 200ms
+RETRANSMIT_INTERVAL_MS = 20  # Time delta between each retransmission attempt
+MAX_RETRANSMIT_ATTEMPTS = 10  # 10 attempts * 20ms = 200ms <= timeout t
+RELIABLE_CHANNEL = 0
+UNRELIABLE_CHANNEL = 1
+
+class Sender:
+    def __init__(self, src_socket_addr, dest_socket_addr):
+        self.src_socket_addr = src_socket_addr
+        self.dest_socket_addr = dest_socket_addr
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(src_socket_addr)
+        self.sock.setblocking(False)
+
+        self.seq_to_send = 0
+        self.pending_acks = {}  # sent_seq -> (sent_time, num retransmit attempts, payload)
+        self.pending_acks_lock = threading.Lock()
+
+        self.running_threads = True
+        self.recv_ack_thread = threading.Thread(target=self._recv_ack, daemon=True)
+        self.retransmit_thread = threading.Thread(target=self._retransmit, daemon=True)
+        self.recv_ack_thread.start()
+        self.retransmit_thread.start()
+
+    def send(self, payload: bytes, is_reliable) -> int:
+        ch = RELIABLE_CHANNEL if is_reliable else UNRELIABLE_CHANNEL
+        seq = self.seq_to_send
+        packet = pack_packet(ch, seq, now_ms(), payload)
+        self.sock.sendto(packet, self.dest_socket_addr)
+        if is_reliable:
+            with self.pending_acks_lock:
+                self.pending_acks[seq] = {
+                    "packet": packet,
+                    "sent_time": now_ms(),
+                    "attempts": 1
+                }
+        self.seq_to_send = self._increment_seq(seq)
+        return seq
+
+    def close(self):
+        self.running_threads = False
+        self.sock.close()
+    
+    def _increment_seq(self, seq_to_send):
+        return (seq_to_send + 1) & 0xFFFF
+
+    def _recv_ack(self):
+        while self.running_threads:
+            try:
+                data, _ = self.sock.recvfrom(65536)
+            except BlockingIOError:
+                time.sleep(0.001)
+                continue
+            try:
+                ch, seq, ts, payload = unpack_packet(data)
+            except Exception:
+                continue
+            
+            if ch == UNRELIABLE_CHANNEL or not payload.startswith(b"ACK"):
+                continue
+            with self.pending_acks_lock:
+                self.pending_acks.pop(seq, None)
+
+    def _retransmit(self):
+        while self.running_threads:
+            time.sleep(0.01)
+            now = now_ms()
+            to_retransmit = []
+            with self.pending_acks_lock:
+                for seq, info in list(self.pending_acks.items()):
+                    if now - info["sent_time"] <= RETRANSMIT_INTERVAL_MS:
+                        continue
+                    if info["attempts"] < MAX_RETRANSMIT_ATTEMPTS:
+                        to_retransmit.append(seq)
+                    else:
+                        del self.pending_acks[seq]
+            for seq in to_retransmit:
+                with self.pending_acks_lock:
+                    info = self.pending_acks.get(seq)
+                    if not info:
+                        continue
+                    self.sock.sendto(info["packet"], self.dest_socket_addr)
+                    info["sent_time"] = now_ms()
+                    info["attempts"] += 1
+                    print(f"[SENDER] Retransmit seq={seq} attempt={info['attempts']}")
+
+# Main sender logic
+sender = Sender(('127.0.0.1', 12000), ('127.0.0.1', 11000))
 start = time.time()
 
 try:
     while time.time() - start < 10:
         is_reliable = random.random() < 0.5
-        # is_reliable = False
         msg = f"hello_{'R' if is_reliable else 'U'}".encode()
-        seq = api.send(msg, is_reliable=is_reliable)
+        seq = sender.send(msg, is_reliable=is_reliable)
         print(f"[SENDER] Sent seq={seq} is_reliable={is_reliable}")
         time.sleep(0.1)
 except KeyboardInterrupt:
     pass
 finally:
-    api.close()
+    sender.close()
